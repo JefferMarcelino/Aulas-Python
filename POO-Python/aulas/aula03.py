319c598a26cd3d509a09bb55f7c80337d1e60965
Jeffer Marcelino <jeffersunde72@gmail.com>
2021-09-26 10:57:04 +0200

    Terminando a aula 03

--- POO-Python/aulas/aula03.py
+++ POO-Python/aulas/aula03.py
@@ -1,19 +1,23 @@
 class Televisão:
-    def __init__(self):
+    def __init__(self, min, max):
         self.ligada = False
         self.canal = 2
+        self.cmin = min
+        self.cmax = max
     
     def muda_canal_para_baixo(self):
-        self.canal -=1
+        if self.canal - 1 >= self.cmin:
+            self.canal -= 1
     
     def muda_canal_para_cima(self):
-        self.canal +=1
+        if self.canal + 1 <= self.cmax:
+            self.canal += 1
 
 
-tv = Televisão()
-tv.muda_canal_para_cima()
-tv.muda_canal_para_cima()
+tv = Televisão(1, 99)
+for x in range(0, 120):
+    tv.muda_canal_para_cima()
 print(tv.canal)
-
-tv.muda_canal_para_baixo()
+for x in range(0, 120):
+    tv.muda_canal_para_baixo()
 print(tv.canal)
